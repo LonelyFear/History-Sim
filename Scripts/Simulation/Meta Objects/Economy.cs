b166137b6933371181bd04e943ee1b08a9d3b659
LonelyFear <152335919+LonelyFear@users.noreply.github.com>
2025-04-08 21:58:38 -0400

    Update Economy.cs

--- Scripts/Simulation/Meta Objects/Economy.cs
+++ Scripts/Simulation/Meta Objects/Economy.cs
@@ -7,44 +7,46 @@ public partial class Economy : GodotObject
 {
     public Dictionary<SimResource, double> resources;
     
-    public void AddResources(SimResource resource, double amount){
-        if (!resources.ContainsKey(resource)){
-            resources.Add(resource, amount);
-        } else {
-            resources[resource] += amount;
-        }
-    }
-
-    public bool EconomyHasResourceType(ResourceType type){
-        foreach (var pair in resources){
-            SimResource resource = pair.Key;
-            double amount = pair.Value;
-            if (amount > 0 && resource.types.Contains(type)){
-                return true;
-            }
-        }
-        return false;  
-    }
-
-    public bool EconomyHasResource(string id){
-        foreach (var pair in resources){
-            SimResource resource = pair.Key;
-            double amount = pair.Value;
-            if (amount > 0 && resource.id == id.ToLower()){
-                return true;
-            }
-        }
-        return false;  
-    }
-
-    public void RemoveResources(SimResource resource, double amount){
-        if (resources.ContainsKey(resource)){
+    public void ChangeResourceAmount(SimResource resource, double amount){
+        if (amount > 0){
+            if (!resources.ContainsKey(resource)){
+                resources.Add(resource, amount);
+            } else {
+                resources[resource] += amount;
+            }            
+        } else if (resources.ContainsKey(resource)){
             resources[resource] -= amount;
             if (resources[resource] <= 0){
                 resources.Remove(resource);
             }
-        } else {
-            GD.PrintErr("Tried to remove resource that doesn't exist from economy");
         }
+        //return 0;
+    }
+
+    public void SetResourceAmount(SimResource resource, double amount){
+        if (amount > 0){
+            if (!resources.ContainsKey(resource)){
+                resources.Add(resource, amount);
+            } else {
+                resources[resource] = amount;
+            }                
+        } else if (resources.ContainsKey(resource)){
+            resources.Remove(resource);
+        }
+    }
+
+    public double AmountOfType(ResourceType type){
+        double totalAmount = 0;
+        foreach (var pair in resources){
+            SimResource resource = pair.Key;
+            if (resource.types.Contains(type)){
+                totalAmount += pair.Value;                
+            }
+        }
+        return totalAmount;
+    }
+
+    public double AmountOfResource(SimResource resource){
+        return resources[resource];
     }
 }
